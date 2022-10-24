4328e27fd842264e41979302d73f0e8ab79e6230
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-10-24 17:33:21 -0300

    ...

--- trybe_course/src/migrations/20221020201107-add-column-course-id-in-table-students.js
+++ trybe_course/src/migrations/20221020201107-add-column-course-id-in-table-students.js
@@ -1,21 +1,21 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('students', 'course_id', { 
-      allowNull: false,
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'courses', // nome da tabela que essa coluna é FK
-        key: 'id'
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    });
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     await queryInterface.addColumn('students', 'course_id', { 
+//       allowNull: false,
+//       type: Sequelize.INTEGER,
+//       references: {
+//         model: 'courses', // nome da tabela que essa coluna é FK
+//         key: 'id'
+//       },
+//       onUpdate: 'CASCADE',
+//       onDelete: 'CASCADE',
+//     });
      
-  },
+//   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('students', 'course_id');
-  }
-};
+//   async down (queryInterface, Sequelize) {
+//     await queryInterface.removeColumn('students', 'course_id');
+//   }
+// };
