5f32a8babefb4606376bb3ff2730b306a2fe9a1b
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-10-24 16:37:25 -0300

    ...

--- trybe_course/src/models/student.js
+++ trybe_course/src/models/student.js
@@ -1,22 +1,22 @@
-module.exports = (sequelize, DataTypes) => {
-  const Student = sequelize.define('Student', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    birthday: DataTypes.DATE,
-    active: DataTypes.BOOLEAN,
-    courseId: DataTypes.INTEGER,
-  }, {
-    underscored: true,
-    timestamps: false,
-  });
+// module.exports = (sequelize, DataTypes) => {
+//   const Student = sequelize.define('Student', {
+//     name: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//     birthday: DataTypes.DATE,
+//     active: DataTypes.BOOLEAN,
+//     courseId: DataTypes.INTEGER,
+//   }, {
+//     underscored: true,
+//     timestamps: false,
+//   });
 
-  Student.associate = (models) => {
-    Student.belongsTo(models.Course, {
-      foreignKey: 'course_id',
-      as: 'curso',
-    })
-  };
+//   Student.associate = (models) => {
+//     Student.belongsTo(models.Course, {
+//       foreignKey: 'course_id',
+//       as: 'curso',
+//     })
+//   };
 
-  return Student;
-};
+//   return Student;
+// };
 
